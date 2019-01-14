27155b48b92619404bc6b778b217d2e2b91a3382
tomasli <tongzhegnng.li@carloha.com>
2019-01-14 18:29:54 +0800

    Update 2_1.py
    2_1 done

--- 2_1.py
+++ 2_1.py
@@ -3,3 +3,7 @@ import re
 print(re.split(r'[;,\s]\s*', line))
 fields = re.split(r'(;|,|\s)\s', line)
 print(fields)
+values = fields[::2]
+delimiters = fields[1::2] + ['']
+print(values)
+print(delimiters)
