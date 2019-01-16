366f7b5d26ed17ae45e83fe3b01ce59bdf134c26
Tomas Li <tongzhegnng.li@carloha.com>
2019-01-16 11:35:39 +0800

    Update 2_2.py

--- 2_2.py
+++ 2_2.py
@@ -5,3 +5,16 @@ print(filename.startswith('file:'))
 
 url = 'http://www.python.org'
 print(url.startswith('http:'))
+
+import os
+filenames = os.listdir('.')
+print(filename)
+
+from urllib.request import urlopen
+
+def read_data(name):
+    if name.startswith(('http:', 'https:', 'ftp:')):
+        return urlopen(name).read()
+    else:
+        with open(name) as f:
+            return f.read()
