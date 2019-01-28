b0ba3e6023d953da183353fcf8bde3c4ef9d56c8
tomasli <litongzheng2009@gmail.com>
2019-01-28 23:13:22 +0800

    2_14 done.

--- 2_14.py
+++ 2_14.py
@@ -2,3 +2,9 @@ parts = ['Is', 'Chicago', 'Not', 'Chicago?']
 print(' '.join(parts))
 print(','.join(parts))
 print(''.join(parts))
+
+a = 'Is Chicago'
+b = 'Not Chicago?'
+print(a + ' ' + b)
+print('{} {}'.format(a, b))
+print('Hello' 'World')
