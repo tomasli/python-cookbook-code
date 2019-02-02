b95cdb2c2d5f82d72fd43f287ec291062adac1da
tomasli <litongzheng2009@gmail.com>
2019-02-02 13:53:22 +0800

    remove file outside folder.

--- 2_1.py
+++
@@ -1,9 +0,0 @@
-line = 'asdf fjdk; afed, fjek,asdf,     foo'
-import re
-print(re.split(r'[;,\s]\s*', line))
-fields = re.split(r'(;|,|\s)\s', line)
-print(fields)
-values = fields[::2]
-delimiters = fields[1::2] + ['']
-print(values)
-print(delimiters)
--- 2_10.py
+++
@@ -1,4 +0,0 @@
-import re
-num = re.compile('\d+')
-print(num.match('123'))
-print(num.match('\u0661\u0662\u0663'))
